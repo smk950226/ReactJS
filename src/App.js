b593d6f6156b9507264c6c96a442d0079bcefda9
smk950226 <smk950226@naver.com>
2018-09-08 22:49:55 +0900

    데이터 이중상속

--- src/App.js
+++ src/App.js
@@ -10,14 +10,21 @@ const movieTitles = [
   "Star Wars"
 ]
 
+const movieImages = [
+  "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} />
-        <Movie title={movieTitles[1]} />
-        <Movie title={movieTitles[2]} />
-        <Movie title={movieTitles[3]} />
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -5,7 +5,7 @@ class Movie extends Component{
     render(){
         return(
             <div>
-                <MoviePoster />
+                <MoviePoster poster={ this.props.poster } />
                 <h1>{ this.props.title }</h1>
             </div>
         )
@@ -15,7 +15,7 @@ class Movie extends Component{
 class MoviePoster extends Component{
     render(){
         return(
-            <img src="https://steamcdn-a.akamaihd.net/steam/apps/416560/header.jpg?t=1504213621" />
+            <img src={ this.props.poster } />
         )
     }
 }
