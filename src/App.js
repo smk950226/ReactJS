aed80fdd681696babf89b6c238386b4d1c83c691
smk950226 <smk950226@naver.com>
2018-09-09 02:36:04 +0900

    ajax

--- src/App.js
+++ src/App.js
@@ -4,37 +4,10 @@ import Movie from './Movie';
  
 class App extends Component {
 
-  state = {
-    
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-          },
-          {
-            title: "StarWars",
-            poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"
-          },
-          {
-            title: "Trainspotting",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg"
-          }
-        ]
-      })
-    }, 3000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort+by=rating')
   }
 
   _renderMovies = () => {
