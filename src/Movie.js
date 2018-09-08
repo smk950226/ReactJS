b7b85e902ef8757d2c7f19747d351a4d2aa0d0c5
smk950226 <smk950226@naver.com>
2018-09-08 23:30:39 +0900

    Prop Validation 추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'; 
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         return(
             <div>
