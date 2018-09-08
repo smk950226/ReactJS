6b47260fb619fdcaffdd05cf69ef2d5cbcc3c672
smk950226 <smk950226@naver.com>
2018-09-09 02:22:27 +0900

    Movie Component 변경

--- src/Movie.js
+++ src/Movie.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react'; 
+import React from 'react'; 
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={ this.props.poster } />
-                <h1>{ this.props.title }</h1>
-            </div>
-        )
-    }
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={ poster } />
+            <h1>{ title }</h1>
+        </div>
+    )
 }
 
 function MoviePoster({poster}){
     return (
         <img src={poster} alt="" />
     )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
