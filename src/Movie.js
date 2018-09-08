50e687b3c4cc95c912fa48632d1a553e602d3ad4
smk950226 <smk950226@naver.com>
2018-09-08 23:34:29 +0900

    MoviePoster에 validation 추가

--- src/Movie.js
+++ src/Movie.js
@@ -20,6 +20,11 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         return(
             <img src={ this.props.poster } alt="" />
